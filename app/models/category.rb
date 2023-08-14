0feba0b54ab789ca3615f6c9492b9dbfc521c4e4
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-14 20:16:43 +0000

    Add destroy dependent

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   belongs_to :user
-  has_many :category_details
+  has_many :category_details, dependent: :destroy
   has_many :details, through: :category_detail
 
   validates :name, presence: true
