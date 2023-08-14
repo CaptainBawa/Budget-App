d109740d060e95bea7d35eb3a3395aa8359f4745
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-14 19:24:55 +0000

    Setup devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,10 @@
 class User < ApplicationRecord
-  has_many :details
-  has_many :categories
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  has_many :details
+  has_many :categories
 
   validates :name, presence: true
   validates :email, presence: true
