ec4b5f322761fdc17c9b2e725e8070206c58ed23
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-14 20:30:49 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :categories
-  resources :details
-  resources :users
+  # resources :categories
+  # resources :details
+  # resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  authenticated :user do
+    root 'categories#index', as: :authenticated_root
+  end
+  unauthenticated do
+    root "splash_screen#index", as: :unauthenticated_root
+  end
+  resources :categories, only: [:index, :new, :show, :create] do
+    resources :details, only: [:index, :new, :create]
+  end
 end
