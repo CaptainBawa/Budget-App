d2b39dd04fc0b77f4f997212bce72e3d19e70353
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-15 19:39:46 +0000

    Fix association

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_many :category_details, dependent: :destroy
-  has_many :details, through: :category_detail
+  has_many :details, through: :category_details
 
   validates :name, presence: true
   validates :icon, presence: true
